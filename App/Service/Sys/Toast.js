24c61d53f8a50baed06670425d87ff0ad1441ca1
qlover <myused@sina.com>
2020-09-01 19:21:47 +0800

    fix:bug: toast

--- App/Service/Sys/Toast.js
+++ App/Service/Sys/Toast.js
@@ -1,16 +1,6 @@
 import Popup from "../../Contracts/Popup";
-import { Toast as NativeToast } from "native-base/src/basic/Toast";
-import {} from '@codler/react-native-keyboard-aware-scroll-view/lib/KeyboardAwareHOC'
 export default class Toast extends Popup {
   constructor() {
     super();
-  }
-
-  showText(text) {
-    // alert(options.message);
-    NativeToast.show({ text });
-  }
-  show(options) {
-    NativeToast.show({ text: options.message });
   }
 }
